728ad02a61ed1afa89a3c563e83bca20ed5cec3c
Rutwik Deshmukh <rutwik.deshmukh@blazeclan.com>
2024-04-28 20:40:56 +0530

    Updating manifest

--- scripts/CheckServices.sh
+++ scripts/CheckServices.sh
@@ -1,7 +1,14 @@
+echo "Docker Version:"
 docker --version
+echo "AWSCLI Version:"
 aws --version
+echo "Node Version:"
 node -v
+echo "NPM Version:"
 npm -v
+echo "Jenkins Status Version:"
 systemctl status jenkins | grep Active
+echo "Trivy Version:"
 trivy -v
+echo "KUBECTL Version:"
 kubectl version --client
