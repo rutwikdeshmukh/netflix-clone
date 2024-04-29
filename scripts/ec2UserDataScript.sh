a7cfb2139a1e4f3ba31ce963a0f841c78db94912
Rutwik Deshmukh <rutwik.deshmukh@blazeclan.com>
2024-04-29 12:51:16 +0530

    Updating

--- scripts/ec2UserDataScript.sh
+++ scripts/ec2UserDataScript.sh
@@ -1,4 +1,5 @@
 #AMAZON LINUX 2023
+#!/bin/bash
 yum update -y
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -8,15 +9,12 @@ dnf install java-17-amazon-corretto -y
 yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
-          
+mount -o remount,size=10G /tmp/
 yum install docker -y
 systemctl start docker
 chmod 666 /var/run/docker.sock
 systemctl enable docker
 yum install -y git
-systemctl status jenkins
-docker --version
-mount -o remount,size=10G /tmp/
 yum install npm -y
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl.sha256
@@ -27,4 +25,17 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$P
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 git clone https://github.com/rutwikdeshmukh/netflix-clone.git
 cd netflix-clone
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+# Ubuntu User Data Script
+#!/bin/bash
+sudo su
+git clone https://github.com/rutwikdeshmukh/netflix-clone.git
+cd netflix-clone
+apt-get install dkpg
+bash scripts/InstallAWSCLI.sh
+bash scripts/InstallKUBECTL.sh
+bash scripts/InstallDocker.sh
+bash scripts/InstallJenkins.sh
+bash scripts/InstallTrivy.sh
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
